8be4d2dd281ea93803784dd723f534e6f91fad4e
autt <foo@bar.com>
2025-02-13 14:41:09 -0600

    win setup

--- pynes/common.py
+++ pynes/common.py
@@ -1,3 +1,4 @@
 import os
 
-ENV_NL = "\n" if os.name != 'nt' else "\r\n"
+IS_WINDOWS = os.name == 'nt'
+ENV_NL = "\n" if not IS_WINDOWS else "\r\n"
--- pynes/reader.py
+++ pynes/reader.py
@@ -1,24 +1,45 @@
 import os, sys
-from typing import TextIO, Callable, Iterable
-from .common import ENV_NL
+from typing import Iterable
+from .common import ENV_NL, IS_WINDOWS
 
-def create_reader(file_descriptor_id:int=None, max_read_buffer=1024):
+def create_reader(file_descriptor_id:int=None, nix_max_read_buffer=1024):
   if file_descriptor_id is None: file_descriptor_id = sys.stdin.fileno()
-
   r = os.fdopen(file_descriptor_id, mode='r')
-  buffArr = bytearray(max_read_buffer)
-  buffArrCont = [buffArr]
 
-  def readlines() -> Iterable:
-    if r.readable():
-      while True:
+  def nix_read_factory():
+    buffArr = bytearray(nix_max_read_buffer)
+    buffArrCont = [buffArr]
 
-        bytes_read = os.readv(r.fileno(), buffArrCont)
-        line = buffArr[:bytes_read].decode()
-        #print(f"line-len: {len(line)} bytes: {bytes_read} {buffArr[:bytes_read]}")
-        #if line is None: break
-        if bytes_read == 0: break
-        for l in line.strip().split(ENV_NL):
-          yield l
+    def readlines() -> Iterable:
+      if r.readable():
+        while True:
 
-  return readlines
+          bytes_read = os.readv(r.fileno(), buffArrCont)
+          line = buffArr[:bytes_read].decode()
+          if bytes_read == 0: break
+          for l in line.strip().split(ENV_NL):
+            yield l
+
+    return readlines
+
+  def win_read_factory():
+    pass
+    #
+    # buffArr = bytearray(nix_max_read_buffer)
+    #
+    # def readlines() -> Iterable:
+    #   if r.readable():
+    #     while True:
+    #
+    #       bytes_read = os.read(r.fileno())
+    #       line = buffArr[:bytes_read].decode()
+    #       if bytes_read == 0: break
+    #       for l in line.strip().split(ENV_NL):
+    #         yield l
+    #
+    # return readlines
+
+  if IS_WINDOWS:
+    return win_read_factory()
+  else:
+    return nix_read_factory()
